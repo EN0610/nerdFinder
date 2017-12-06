8809e66c3e54945c30e5a23c721fca2978d95b09
hronchetti <harryronchetti@gmail.com>
2017-12-06 15:09:36 +0000

    Setting up Environment
    Organising the enviroment to work with the database

--- index.php
+++ index.php
@@ -1,11 +1,55 @@
 <!DOCTYPE html>
+
 <html>
 <head>
-    <title></title>
+    <title>Hire Experts to Create Amazing Ideas | Nerd Finder</title>
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/fonts.css" rel="stylesheet">
+    <link href="css/base.css" rel="stylesheet">
 </head>
 <body>
-    hello world
+    <nav class="nav">
+            
+    </nav>
+    <header class="header--big">
+        <article class="wrapper">
+            <h1 class="grid-2">Hire experts to create your apps, websites, software &amp; games</h1>
+            <a class="button-primary button-primary--white" href=""></a>
+            <a class="button-secondary button-secondary--white" href=""></a>
+        </article>
+    </header>
+    <section>
+        <h2></h2>
+        <hr>
+        <article>
+            <img alt="" src="">
+            <h3></h3>
+            <p>
+            </p>
+            <a href=""></a>
+        </article>
+        <article>
+            <img alt="" src="">
+            <h3></h3>
+            <p>
+            </p>
+            <a href=""></a>
+        </article>
+        <article>
+            <img alt="" src="">
+            <h3></h3>
+            <p>
+            </p>
+            <a href=""></a>
+        </article>
+    </section>
+    <section>
+        
+    </section>
+    <section></section>
+    <section></section>
+    <footer>
+        
+    </footer>
 </body>
 </html>
