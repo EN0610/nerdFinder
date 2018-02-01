abb433995ea9918c3be6bdbaf2a00113b4a42f6d
DaleC-88 <dale.clement@outlook.com>
2018-02-01 12:38:49 +0000

    Congratulations

--- congratulations.php
+++ congratulations.php
@@ -8,7 +8,7 @@
 <html lang="en">
 <head>
     <?php require_once('scripts/analytics-tracking.php');?>
-    <title>Hire Experts to Create Amazing Ideas | Nerd Finder</title>
+    <title>Go premium | Nerd Finder</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
     <link rel="stylesheet" href="css/fonts.css">
@@ -16,39 +16,33 @@
 </head>
 <body>
     <?php require_once('elements/nav.php'); ?>
-    <header class="header header--big">
+    <header class="header background-gradient">
         <article class="wrapper">
-            <h1 class="header__heading grid-2">Congratulations!</h1>
+            <h1>congratulations!</h1>
         </article>
     </header>
-    <section>
-        <h2></h2>
-        <hr>
-        <article>
-            <img alt="" src="">
-            <h3></h3>
-            <p>
-            </p>
-            <a href=""></a>
-        </article>
-        <article>
-            <img alt="" src="">
-            <h3></h3>
-            <p>
-            </p>
-            <a href=""></a>
-        </article>
-        <article>
-            <img alt="" src="">
-            <h3></h3>
-            <p>
-            </p>
-            <a href=""></a>
-        </article>
+    <section class="soft-box soft-box--padded wrapper main">
+            <form class="flex-grid full-width-form">
+                <article class="grid-1">
+                    <!-- <h3>What's included</h3>
+                    <ul class="bulleted-list">
+                        <li>Free project processing</li>
+                        <li>Priority ranking in searches</li>
+                    </ul> -->
+                </article>
+                <article class="grid-1">
+                    <h3>You're all signed up</h3>
+                    <h3>You can now begin using your account</h3>
+                    <input class="button button--primary-green center-button" type="submit" name="" value="Sign in" href="sign-in.php">
+                </article>
+                <article class="grid-1">
+                    <!-- <h3>Security code</h3>
+                    <input class="full-width-form__field" type="text" name="security-code">
+                    <h3>Expiration Date</h3>
+                    <input class="full-width-form__field" type="text" name="expiration"> -->
+                </article>
+            </form>
     </section>
-    <section></section>
-    <section></section>
-    <section></section>
     <?php require_once('elements/footer--big.php'); ?>
 </body>
 </html>
