b3bbfdb2d341cc6f8f9abd03a88f6468211f2a91
JacksonINC <32329012+JacksonINC@users.noreply.github.com>
2018-02-03 22:29:22 +0000

    changed project script which was wrong

--- scripts/project-search.php
+++ scripts/project-search.php
@@ -8,3 +8,16 @@ $sql = "SELECT *
 $result = $conn->query($sql);
 
 $projects = "";
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $nerds .= "<br> Project: ". $row["projectname"]. " - What & When: ". $row["projectdescription"]. " " . $row["posted"] . "<br>";
+
+    }
+} else {
+    echo "Currently no projects on!";
+}
+
+$conn->close();
