5145c2179aaa7b6e631a6e564c8b7d3fbd805136
jackbreakell95 <jack.breakell@northumbria.ac.uk>
2018-02-06 16:32:50 +0000

    edit profile pre pop

--- edit-profile.php
+++ edit-profile.php
@@ -1,16 +1,44 @@
 <?php
-    // Defining path to session data folder where all session data will be saved/found
-    require_once('scripts/session-save-path.php');
-    // Resuming current session
-    session_start();
+// Defining path to session data folder where all session data will be saved/found
+require_once('scripts/session-save-path.php');
+// Resuming current session
+session_start();
+// Setting the development enviroment to show errors
+require_once('scripts/set-environment.php');
+// Connecting to the Database
+require_once('scripts/database-connection.php');
+
+    $userid = $_SESSION['userid'];
+    $userid = filter_var($userid, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    $userid = filter_var($userid, FILTER_SANITIZE_SPECIAL_CHARS);
+    $userid = trim($userid);
+    // SQL using the ID in request stream to pull rest of project info from the Database
+    $sql = "SELECT * FROM nf_users WHERE userid = $userid";
+
+    $editprofile = mysqli_query($conn, $sql) or die (mysqli_error($conn));
+
+            while ($row =mysqli_fetch_assoc($editprofile)) {
+              $firstname = $row['firstname'];
+              $lastname = $row['lastname'];
+              $email = $row['email'];
+              $username = $row['username'];
+              $dob = $row['dob'];
+              $userpassword = $row['userpassword'];
+              $passwordhint = $row['passwordhint'];
+              $profilepic = $row['profilepic'];
+              $experience = $row['experience'];
+              $specialismid = $row['specialismid'];
+              $rate = $row['rate'];
+              $nerdcv = $row['nerdcv'];
+              $portfolioimg1 = $row['portfolioimg1'];
+            }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php require_once('scripts/analytics-tracking.php');?>
     <title>Edit profile | Nerd Finder</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+    <link rel="stylesheet" href="css/reset.css">   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="stylesheet" href="css/base.css">
 </head>
@@ -21,57 +49,57 @@
           <h4>Update your personal information</h4>
       </article>
     </header>
-    <section class="wrapper main">
-        <form action="scripts/edit-profile-process-nerd.php" method="get" class="flex-grid">
-            <section class = "soft-box soft-box--padded grid-3-1--small">
+      <section class='wrapper main'>
+        <form action='scripts/edit-profie-process-nerd.php' method='get' class='flex-grid'>
+            <section class = 'soft-box soft-box--padded grid-3-1--small'>
                 <aside>
-                    <h2 class="full-width-form__heading">Your details</h2>
+                    <h2 class='full-width-form__heading'>Your details</h2>
                     <h3>Username</h3>
-                    <input class="full-width-form__field" type="text" name="username" id="username" accesskey="4" tabindex="4" disabled>
+                    <input class='full-width-form__field' type='text' name='username' id='$username' value="<?php echo($username);?>" accesskey='4' tabindex='4' readonly>
                     <h3>Email</h3>
-                    <input class="full-width-form__field" type="email" name="email" id="email" accesskey="3" tabindex="3">
+                    <input class='full-width-form__field' type='email' name='email' id='email' value="<?php echo($email);?>"accesskey='3' tabindex='3'>
                     <h3>Password</h3>
-                    <input class="full-width-form__field" type="password" name="userpassword" id="userpassword" accesskey="6" tabindex="6">
+                    <input class='full-width-form__field' type='password' name='userpassword' id='userpassword' accesskey='6' tabindex='6'>
                     <h3>Password Hint</h3>
-                    <input class="full-width-form__field"type="text" name="passwordhint" id="passwordhint" accesskey="7" tabindex="7">
+                    <input class='full-width-form__field'type='text' name='passwordhint' id='passwordhint' accesskey='7' tabindex='7'>
                     <h3>DOB</h3>
-                    <input class="full-width-form__field" type="date" name="dob" id="dob" accesskey="5" tabindex="5">
+                    <input class='full-width-form__field' type='date' name='dob' id='dob' accesskey='5' tabindex='5'>
                 </aside>
             </section>
-            <section class="soft-box soft-box--padded grid-3-2--small">
-                <h2 class="full-width-form__heading">About you</h2>
+            <section class='soft-box soft-box--padded grid-3-2--small'>
+                <h2 class='full-width-form__heading'>About you</h2>
                 <h3>Profile picture</h3>
-                <img class="user-profile" src="img/profile-pics/no-image.jpg" alt="Profile Picture">
-                <input type="file" name="profilepic" id="profilepic" accesskey="8" tabindex="8" value="Browse">
-                <article class="flex-grid">
-                    <aside class="grid-2-1">
+                <img class='user-profile' src='img/profile-pics/no-image.jpg' alt='Profile Picture'>
+                <input type='file' name='profilepic' id='profilepic' accesskey='8' tabindex='8' value='Browse'>
+                <article class='flex-grid'>
+                    <aside class='grid-2-1'>
                         <h3>Firstname</h3>
-                        <input class="full-width-form__field" type="text" name="firstname" id="firstname" accesskey="1" tabindex="1">
+                        <input class='full-width-form__field' type='text' name='firstname' id='firstname' accesskey='1' tabindex='1'>
                         <h3>Years of experience</h3>
-                        <input class="full-width-form__field" type="number" name="experience" id="experience" accesskey="9" tabindex="9" min="0">
+                        <input class='full-width-form__field' type='number' name='experience' id='experience' accesskey='9' tabindex='9' min='0'>
                         <h3>Specialism</h3>
-                        <select class="full-width-form__field" name="specialismid">
-                            <option value="1">Websites</option>
-                            <option value="2">Mobile Apps</option>
-                            <option value="3">Tablet Apps</option>
-                            <option value="4">Software</option>
-                        </select><span class="icon-arrow-down select-icon"></span>
+                        <select class='full-width-form__field' name='specialismid'>
+                            <option value='1'>Websites</option>
+                            <option value='2'>Mobile Apps</option>
+                            <option value='3'>Tablet Apps</option>
+                            <option value='4'>Software</option>
+                        </select><span class='icon-arrow-down select-icon'></span>
                         <h3>Hourly rate</h3>
-                        <input class="full-width-form__field" type="number" name="rate" id="rate" accesskey="10" tabindex="10" min="0" placeholder="£20">
+                        <input class='full-width-form__field' type='number' name='rate' id='rate' accesskey='10' tabindex='10' min='0' placeholder='£20'>
                     </aside>
-                    <aside class="grid-2-1">
+                    <aside class='grid-2-1'>
                         <h3>Lastname</h3>
-                        <input class="full-width-form__field" type="text" name="lastname" id="lastname" accesskey="2" tabindex="2">
+                        <input class='full-width-form__field' type='text' name='lastname' id='lastname' accesskey='2' tabindex='2'>
                         <h3>Upload CV</h3>
-                        <input class="full-width-form__field" type="file" name="nerdcv" id="nerdcv" accesskey="11" tabindex="11" value="Browse">
+                        <input class='full-width-form__field' type='file' name='nerdcv' id='nerdcv' accesskey='11' tabindex='11' value='Browse'>
                         <h3>Portfolio images</h3>
-                        <input class="portfolioimg" type="file" name="portfolioimg1" id="portfolioimg1" accesskey="11" tabindex="11" value="Browse">
-                        <input class="portfolioimg" type="file" name="portfolioimg2" id="portfolioimg2" accesskey="11" tabindex="11" value="Browse">
-                        <input class="portfolioimg" type="file" name="portfolioimg3" id="portfolioimg3" accesskey="11" tabindex="11" value="Browse">
-
+                        <input class='portfolioimg' type='file' name='portfolioimg1' id='portfolioimg1' accesskey='11' tabindex='11' value='Browse'>
+                        <input class='portfolioimg' type='file' name='portfolioimg2' id='portfolioimg2' accesskey='11' tabindex='11' value='Browse'>
+                        <input class='portfolioimg' type='file' name='portfolioimg3' id='portfolioimg3' accesskey='11' tabindex='11' value='Browse'>
                     </aside>
                 </article>
-                <input type="submit" value="SAVE CHANGES" class="button button--primary-green center-button" accesskey="12" tabindex="12">
+                <input type='submit' value='SAVE CHANGES' class='button button--primary-green center-button' accesskey='12' tabindex='12'>
+                <a class="button button--primary-red" href="scripts/delete-profile-proces.php">DELETE PROFILE</a>&nbsp;&nbsp;
             </section>
         </form>
     </section>
--- scripts/sign-up-process-nerd.php
+++ scripts/sign-up-process-nerd.php
@@ -5,21 +5,63 @@
       die("Connection failed: " . mysqli_connect_error());
   }
 
-  $firstname = $_POST['firstname'];
-  $lastname = $_POST['lastname'];
-  $email = $_POST['email'];
-  $username = $_POST['username'];
-  $dob = $_POST['dob'];
-  $userpassword = $_POST['userpassword'];
-  $passwordhint = $_POST['passwordhint'];
-  $profilepic = $_POST['profilepic'];
-  $experience = $_POST['experience'];
-  $specialismid = $_POST['specialismid'];
-  $rate = $_POST['rate'];
-  $nerdcv = $_POST['nerdcv'];
-  $portfolioimg1 = $_POST['portfolioimg1'];
-  // $portfolioimg2 = $_POST['portfolioimg2'];
-  // $portfolioimg3 = $_POST['portfolioimg3'];
+  // Retriving data from form fields
+  $firstname = isset($_REQUEST['firstname']) ? $_REQUEST['firstname'] : null;
+  $lastname = isset($_REQUEST['lastname']) ? $_REQUEST['lastname'] : null;
+  $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+  $username = isset($_REQUEST['username']) ? $_REQUEST['username'] : null;
+  $dob = isset($_REQUEST['dob']) ? $_REQUEST['dob'] : null;
+  $userpassword = isset($_REQUEST['userpassword']) ? $_REQUEST['userpassword'] : null;
+  $passwordhint = isset($_REQUEST['passwordhint']) ? $_REQUEST['passwordhint'] : null;
+  $profilepic = isset($_REQUEST['profilepic']) ? $_REQUEST['profilepic'] : null;
+  $experience = isset($_REQUEST['experience']) ? $_REQUEST['experience'] : null;
+  $specialismid = isset($_REQUEST['specialismid']) ? $_REQUEST['specialismid'] : null;
+  $rate = isset($_REQUEST['rate']) ? $_REQUEST['rate'] : null;
+  $nerdcv = isset($_REQUEST['nerdcv']) ? $_REQUEST['nerdcv'] : null;
+  $portfolioimg1 = isset($_REQUEST['portfolioimg1']) ? $_REQUEST['portfolioimg1'] : null;
+  // Sanitising data but not encoding quotes
+  $firstname = filter_var($firstname, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $lastname = filter_var($lastname, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $email = filter_var($email, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $username = filter_var($username, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $dob = filter_var($dob, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $userpassword = filter_var($userpassword, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $passwordhint = filter_var($passwordhint, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $profilepic = filter_var($profilepic, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $experience = filter_var($experience, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $specialismid = filter_var($specialismid, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $rate = filter_var($rate, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $nerdcv = filter_var($nerdcv, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $portfolioimg1 = filter_var($portfolioimg1, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  // Escaping <,>,& and all ASCII characters with a value below 32
+  $firstname = filter_var($firstname, FILTER_SANITIZE_SPECIAL_CHARS);
+  $lastname = filter_var($lastname, FILTER_SANITIZE_SPECIAL_CHARS);
+  $email = filter_var($email, FILTER_SANITIZE_SPECIAL_CHARS);
+  $username = filter_var($username, FILTER_SANITIZE_SPECIAL_CHARS);
+  $dob = filter_var($dob, FILTER_SANITIZE_SPECIAL_CHARS);
+  $userpassword = filter_var($userpassword, FILTER_SANITIZE_SPECIAL_CHARS);
+  $passwordhint = filter_var($passwordhint, FILTER_SANITIZE_SPECIAL_CHARS);
+  $profilepic = filter_var($profilepic, FILTER_SANITIZE_SPECIAL_CHARS);
+  $experience = filter_var($experience, FILTER_SANITIZE_SPECIAL_CHARS);
+  $specialismid = filter_var($specialismid, FILTER_SANITIZE_SPECIAL_CHARS);
+  $rate = filter_var($rate, FILTER_SANITIZE_STRING, FILTER_SANITIZE_SPECIAL_CHARS);
+  $nerdcv = filter_var($nerdcv, FILTER_SANITIZE_STRING, FILTER_SANITIZE_SPECIAL_CHARS);
+  $portfolioimg1 = filter_var($portfolioimg1, FILTER_SANITIZE_STRING, FILTER_SANITIZE_SPECIAL_CHARS);
+  // Trimming data to remove and white/ empty space
+  $firstname = trim($firstname);
+  $lastname = trim($lastname);
+  $email = trim($email);
+  $username = trim($username);
+  $dob = trim($dob);
+  $userpassword = trim($userpassword);
+  $passwordhint = trim($passwordhint);
+  $profilepic = trim($profilepic);
+  $experience = trim($experience);
+  $specialismid = trim($specialismid);
+  $rate = trim($rate);
+  $nerdcv = trim($nerdcv);
+  $portfolioimg1 = trim($portfolioimg1);
+  // Any data left by this point will not be harmful to the database
 
   $hashedpassword = password_hash($userpassword, PASSWORD_BCRYPT, array(
     'cost' => 11
