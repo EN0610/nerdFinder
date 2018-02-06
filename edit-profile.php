4aae45c6681fc5b11aed50270246e886150b4ae6
jackbreakell95 <jack.breakell@northumbria.ac.uk>
2018-02-06 16:40:59 +0000

    delete profile

--- edit-profile.php
+++ edit-profile.php
@@ -99,7 +99,7 @@ require_once('scripts/database-connection.php');
                     </aside>
                 </article>
                 <input type='submit' value='SAVE CHANGES' class='button button--primary-green center-button' accesskey='12' tabindex='12'>
-                <a class="button button--primary-red" href="scripts/delete-profile-proces.php">DELETE PROFILE</a>&nbsp;&nbsp;
+                <a class="button button--primary-red" href="scripts/delete-profile-process.php">DELETE PROFILE</a>&nbsp;&nbsp;
             </section>
         </form>
     </section>
