b714327c33159934c94256fa0377e18554d2410f
DaleC-88 <dale.clement@outlook.com>
2018-02-07 15:12:38 +0000

    project new

--- create-project-client.php
+++ create-project-client.php
@@ -23,37 +23,39 @@
       </article>
     </header>
     <section class="soft-box soft-box--padded wrapper main">
-      <h3>Project details</h3>
-      <form id = "feedback" action="scripts/create-project-process.php" method="post" class="flex-grid full-width-form">
-        <article class="grid-1">
-          <h3 for="projectname">NAME</h3>
-          <input type="text" name="projectname" id="projectname" required  accesskey="1" tabindex="1">
-          <h3 for="projectdescription">DESCRIPTION</h3>
-          <input type="text" name="projectdescription" id="projectdescription" required  accesskey="2" tabindex="2">
-        </article>
-        <article class="grid-1">
-          <h3 for="projectspecialistarea">SPECIALIST AREA</h3>
-          <select name="projectspecialistarea" id="projectspecialistarea" required  accesskey="3" tabindex="3">
-            <option value="1">Websites</option>
-            <option value="2">Mobile apps</option>
-            <option value="3">Tablet apps</option>
-            <option value="4">Software</option>
-          </select><span class='icon-arrow-down select-icon'></span>
-          <h3 for="projectdeadline">DEADLINE</h3>
-          <input type="date" name="projectdeadline" id="projectdeadline" required  accesskey="4" tabindex="4">
-          <h3 for="projectbudget">BUDGET</h3>
-          <input type="text" name="projectbudget" id="projectbudget" required  accesskey="5" tabindex="5">
-        </article>
-        <article class="grid-1">
-          <h3 for="inspirationimg1">Inspiration Image 1</h3>
-          <input type="file" name="inspirationimg1" id="inspirationimg1" accesskey="6" tabindex="6">
-          <h3 for="inspirationimg2">Inspiration Image 2</h3>
-          <input type="file" name="inspirationimg2" id="inspirationimg2" accesskey="7" tabindex="7">
-          <h3 for="inspirationimg3">Inspiration Image 3</h3>
-          <input type="file" name="inspirationimg3" id="inspirationimg3" accesskey="8" tabindex="8">
-        </article>
-        <input type="checkbox" name="terms"><p class="terms__text">I agree to the Nerd Finder website service terms and conditions</p>
-        <input type="submit" value="CREATE PROJECT" class="button button--primary-green center-button" accesskey="12" tabindex="12">
+      <h2>Project details</h2>
+      <form class="flex-grid full-width-form" id = "feedback" action="scripts/create-project-process.php" method="post">
+      <article class="grid-1">
+            <h3 for="projectname">NAME</h3>
+            <input class="full-width-form__field" type="text" name="projectname" id="projectname" required  accesskey="1" tabindex="1">
+            <h3 for="projectdescription">DESCRIPTION</h3>
+            <input class="full-width-form__field" type="text" name="projectdescription" id="projectdescription" required  accesskey="2" tabindex="2">
+      </article>
+      <article class="grid-1">
+        <h3 for="projectspecialistarea">SPECIALIST AREA</h3>
+        <select class="full-width-form__field" name="projectspecialistarea" id="projectspecialistarea" required  accesskey="3" tabindex="3">
+          <option value="1">Websites</option>
+          <option value="2">Mobile apps</option>
+          <option value="3">Tablet apps</option>
+          <option value="4">Software</option>
+        </select><span class='icon-arrow-down select-icon'></span>
+        <h3 for="projectdeadline">DEADLINE</h3>
+        <input class="full-width-form__field" type="date" name="projectdeadline" id="projectdeadline" required  accesskey="4" tabindex="4">
+        <h3 for="projectbudget">BUDGET</h3>
+        <input class="full-width-form__field" type="text" name="projectbudget" id="projectbudget" required  accesskey="5" tabindex="5">
+      </article>
+      <article class="grid-1">
+        <h3 for="inspirationimg1">Inspiration Image 1</h3>
+        <input class="full-width-form__field" type="file" name="inspirationimg1" id="inspirationimg1" accesskey="6" tabindex="6">
+        <h3 for="inspirationimg2">Inspiration Image 2</h3>
+        <input class="full-width-form__field" type="file" name="inspirationimg2" id="inspirationimg2" accesskey="7" tabindex="7">
+        <h3 for="inspirationimg3">Inspiration Image 3</h3>
+        <input class="full-width-form__field" type="file" name="inspirationimg3" id="inspirationimg3" accesskey="8" tabindex="8">
+      </article>
+      <article>
+      <input type="checkbox" name="terms"><p class="terms__text">I agree to the Nerd Finder website service terms and conditions</p>
+      <input type="submit" value="CREATE PROJECT" class="button button--primary-green center-button" accesskey="12" tabindex="12">
+    </article>
       </form>
     </section>
     <?php require_once('elements/footer--big.php'); ?>
