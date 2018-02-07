01321b506df191a5eddc732468eae86879611626
JacksonINC <32329012+JacksonINC@users.noreply.github.com>
2018-02-07 15:26:07 +0000

    changed scripts - fixes and links

--- scripts/nerd-search.php
+++ scripts/nerd-search.php
@@ -5,7 +5,8 @@ require_once('set-environment.php');
 require_once('database-connection.php');
 
 $nerdsql = "SELECT * 
-            FROM nf_users 
+            FROM nf_users INNER JOIN nf_specialismtypes
+            ON nf_users.specialismid = nf_specialismtypes.specialismid
             WHERE usertypeid = 2";
 
 $nerdResult = $conn->query($nerdsql);
@@ -19,12 +20,13 @@ if ($nerdResult->num_rows > 0) {
         $userid = $row["userid"];
         $firstname = $row["firstname"];
         $lastname = $row["lastname"];
+        $specialismdesc = $row["specialismdesc"];
         $profilepic = $row["profilepic"];
-        $exp = $row ["experience"];
-        $hourlyrate = $row ["hourlyrate"];
-        $portfolioImg1 = $row ["portfolioimg1"];
-        $portfolioImg2 = $row ["portfolioimg2"];
-        $portfolioImg3 = $row ["portfolioimg3"];
+        $exp = $row["experience"];
+        $hourlyrate = $row["hourlyrate"];
+        $portfolioImg1 = $row["portfolioimg1"];
+        $portfolioImg2 = $row["portfolioimg2"];
+        $portfolioImg3 = $row["portfolioimg3"];
 
         $nerds .= <<<NERDS
 
@@ -35,7 +37,7 @@ if ($nerdResult->num_rows > 0) {
                 <img class="user-box_pic" src="img/portfolios/$portfolioImg3" alt="Portfolio Image">
                 <img class="user-box_profilepic" src="img/profile-pics/$profilepic" alt="User Box Picture">
                 <h2 class="user-box_name">$firstname $lastname</h2>
-                <p class="user-box_experience"> $exp years experience</p>
+                <p class="user-box_experience"> $specialismdesc <br> $exp years experience</p>
                 <p class="user-box_hourlyrate"> £$hourlyrate/ph</p>
                 <div class="clearfix"></div>
             </article>
